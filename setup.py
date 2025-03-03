e5b7d4d108c93845d6905620bb9456b0467e140f
Indira Ocampo Justiniano <indira.ocampo@csic.es>
2025-03-03 13:03:09 +0100

    setup file update

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CMBFeatureNet",
+    version="0.1.0",
+    author="Indira Ocampo",
+    author_email="indira.ocampo@csic.es",
+    description="A Python package for CMB data simulation.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/IndiraOcampo/CMBFeatureNet",
+    packages=find_packages(),
+    install_requires=[
+        "numpy", "scipy", "matplotlib",  # Add dependencies here
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
